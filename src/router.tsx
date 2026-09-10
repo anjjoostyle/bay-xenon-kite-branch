e00ace8a697f105366730c16afb7ac1035e22742
anjjoostyle <anjjoostyle@gmail.com>
2026-09-10 17:45:42 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
+import "tslib";
 import { createRouter } from "@tanstack/react-router";
 import { AppErrorComponent } from "@/lib/error-component";
 import { routeTree } from "./routeTree.gen";
-
 export function getRouter() {
   return createRouter({ routeTree, defaultErrorComponent: AppErrorComponent });
 }
